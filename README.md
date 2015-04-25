93c56f63b0e5594ea0ebcb2f5b21d776a42ebd54
Lars Kuhtz <lars@alephcloud.com>
2015-04-24 22:52:07 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,76 @@
+It seems that
+[`Data.Text.toCaseFold`](https://hackage.haskell.org/package/text-1.2.0.4/docs/Data-Text.html#g:8)
+can cause long compilation times with recent versions of GHC. This function is
+also used in the
+[FoldCase](https://hackage.haskell.org/package/case-insensitive-1.2.0.4/docs/Data-CaseInsensitive.html#t:FoldCase)
+instance for `Data.Text.Text`.
+
+The code used for running the tests is [here](ToCaseFoldBuildTimes.hs).
+
+Simple Test
+===========
+
+First we test the build of the following simple Haskell application with
+different versions of GHC.
+
+```haskell
+{-# LANGUAGE OverloadedStrings #-}
+module Main
+
+import Data.Text
+
+main ∷ IO ()
+main = do
+    -- the following line is repeated n times:
+    print (toCaseFold "a")
+```
+
+With `-O0` the build times scale constant in the number of calls to
+`toCaseFold` in the example program, which indicates that the build times are
+dominated by other factors and not the compilation of the `toCaseFold`
+statement. GHC-7.8.3 shows by far the worst build times. Build times with
+GHC-7.10.1 are almost back to where they are with GHC-7.6.3.
+
+![Results with -O0 (see below)](src/results-O0.png?raw=true "Results with -O0")
+
+With `-O` the build times scale linearly with the number of calls to
+`toCaseFold` for all three compiler versions, which indicates that the build
+times are dominated by calls to `toCaseFold`. The linear factor is by far the
+smallest for GHC-7.6.3. The factor with GHC-7.10.1 is somewhat larger than with
+GHC-7.8.3.
+
+![Results with -O (see below)](src/results-O.png?raw=true "Results with -O")
+
+With `-O2` the situation similar to the `-O` case but the compilation times are
+much worse:
+
+![Results with -O2 (see below)](src/results-O2.png?raw=true "Results with -O2")
+
+Using `Data.Text.toLower` instead of `Data.Text.toCaseFold` reduces the build
+times considerably so that they scale constant in the number of calls to
+`toCaseFold` (at least for the values that we considered).
+
+Nested Test
+===========
+
+Next we test the build of the following Haskell application with different
+version of GHC.
+
+```haskell
+{-# LANGUAGE OverloadedStrings #-}
+module Main
+( main
+) where
+
+import qualified Data.Text as T
+
+main :: IO ()
+main = print
+    -- the following line is repeated n times:
+    $ T.toCaseFold . T.take 1
+    $ "abc"
+```
+
+
+
+
---
+++ ToCaseFoldBuildTimes.hs
@@ -0,0 +1,158 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE UnicodeSyntax #-}
+
+-- | Plot output with gnuplot as follows:
+--
+-- > set xlabel "number of calls to Data.Text.toCaseFold"
+-- > set ylabel "build time in seconds"
+-- > set title "Build with -O"
+-- > set label "main ∷ IO ()\nmain =\n    -- repeated n times:\n    print (toCaseFold \"a\")" at 2,30
+-- > plot "results.txt" index 0 with lines title "ghc-7.6.3", "results.txt" index 1 with lines title "ghc-7.8.3", "results.txt" index 2 with lines title "ghc-7.10.1"
+--
+module Main
+( main
+) where
+
+import Data.Monoid
+import Data.String
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+import Distribution.Simple.Utils
+import Distribution.Verbosity
+
+import Numeric.Natural
+
+import System.Clock
+import System.FilePath
+import System.Environment
+import System.Process
+
+-- -------------------------------------------------------------------------- --
+-- static parameters
+
+optimizationFlag ∷ IsString a ⇒ a
+optimizationFlag="-O"
+
+defaultVersions ∷ IsString a ⇒ [a]
+defaultVersions = ["7.6.3", "7.8.3", "7.10.1"]
+
+sizeList ∷ [Natural]
+sizeList = [1..10] <> [20]
+
+app
+    ∷ Natural
+        -- ^ number of times 'T.toFoldCase' is called
+    → T.Text
+app = app1
+
+-- -------------------------------------------------------------------------- --
+-- main
+
+main ∷ IO ()
+main = do
+    versions ← getArgs >>= \case
+        [] → return defaultVersions
+        x → return $ T.pack <$> x
+    mapM_ (benchGhcVersion sizeList) versions
+
+benchGhcVersion
+    ∷ [Natural]
+        -- ^ list with test cases (number of times 'T.toFoldCase' is called)
+    → T.Text
+        -- ^ compiler version
+    → IO ()
+benchGhcVersion l ver = do
+    T.putStrLn $ "# ghc-" <> ver
+    mapM_ (\i → bench i ver >>= printTime i) l
+    T.putStrLn "\n"
+  where
+    printTime i t = T.putStrLn $  sshow i <> " " <> sshow (fromIntegral (timeSpecAsNanoSecs t) / 1e9 ∷ Double)
+
+bench
+    ∷ Natural
+        -- ^ size
+    → T.Text
+        -- ^ compiler version
+    → IO TimeSpec
+bench size ver = withTempDirectory normal "." "ghc-bench" $ \dir → do
+    let fileName = dir </> "Main.hs"
+    T.writeFile fileName $ app size
+    compile ver fileName
+
+compile
+    ∷ T.Text
+        -- ^ compiler version
+    → FilePath
+        -- ^ Filename
+    → IO TimeSpec
+compile ver fileName = fmap fst . time $
+    callCommand $ "ghc-" <> T.unpack ver <> " -fforce-recomp " <> optimizationFlag <> " " <> fileName <> " >/dev/null"
+
+-- |
+-- > main = do
+-- >    -- repeat the following statement n times
+-- >    print (toCaseFold "a")
+--
+app0
+    ∷ Natural
+        -- ^ number of times 'T.toFoldCase' is called
+    → T.Text
+app0 n = T.unlines $
+    [ "{-# LANGUAGE OverloadedStrings #-}"
+    , "module Main"
+    , "( main"
+    , ") where"
+    , ""
+    , "import qualified Data.Text as T"
+    , ""
+    , "main :: IO ()"
+    , "main = do"
+    ]
+    <> replicate (fromIntegral n) "    print (toCaseFold \"a\")"
+    -- <> replicate (fromIntegral n) "    print (toLower \"a\")"
+
+-- |
+-- > import qualified Data.Text as T
+-- >
+-- > main = print
+-- >     -- repeat following statement n times
+-- >     $ T.toCaseFold . T.take 1
+-- >     $ "abc"
+--
+app1
+    ∷ Natural
+        -- ^ number of times 'T.toFoldCase' is called
+    → T.Text
+app1 n = T.unlines $
+    [ "{-# LANGUAGE OverloadedStrings #-}"
+    , "module Main"
+    , "( main"
+    , ") where"
+    , ""
+    , "import qualified Data.Text as T"
+    , ""
+    , "main :: IO ()"
+    , "main = print"
+    ]
+    <> replicate (fromIntegral n) "    $ T.toCaseFold . T.take 1"
+    <> [ "    $ \"abc\"" ]
+
+-- -------------------------------------------------------------------------- --
+-- Utils
+
+sshow ∷ (Show a, IsString b) ⇒ a → b
+sshow = fromString . show
+
+time
+    ∷ IO a
+    → IO (TimeSpec, a)
+time action = do
+  start ← getTime Monotonic
+  !result ← action
+  end ← getTime Monotonic
+  let !delta = diffTimeSpec start end
+  return (delta, result)
+
