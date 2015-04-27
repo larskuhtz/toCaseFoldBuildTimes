536ce71bccf3b4b2e6a016d491bf90200b9f18e9
Lars Kuhtz <lars@alephcloud.com>
2015-04-27 00:55:17 -0700

    update gnuplot and haskell code

--- ToCaseFoldBuildTimes.hs
+++ ToCaseFoldBuildTimes.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE UnicodeSyntax #-}
 
 -- | Plot output with gnuplot as follows:
@@ -15,82 +18,242 @@ module Main
 ( main
 ) where
 
-import Data.Monoid
+import qualified Options.Applicative as O
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Error.Class
+
+import Data.Monoid.Unicode
 import Data.String
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import Data.Typeable
 
 import Distribution.Simple.Utils
 import Distribution.Verbosity
 
 import Numeric.Natural
 
+import Prelude.Unicode
+
 import System.Clock
 import System.FilePath
-import System.Environment
+import System.Posix.Files
 import System.Process
+
+import qualified Text.Parsec as P
+import qualified Text.Parsec.Token as P
+import qualified Text.Parsec.Language as P (haskellDef)
 
 -- -------------------------------------------------------------------------- --
 -- static parameters
 
-optimizationFlag ∷ IsString a ⇒ a
-optimizationFlag="-O2"
-
-defaultVersions ∷ IsString a ⇒ [a]
-defaultVersions = ["7.6.3", "7.8.3", "7.10.1"]
+defaultVersions ∷ [GhcVersion]
+defaultVersions = [GHC_7_6_3, GHC_7_8_3, GHC_7_10_1]
 
 sizeList ∷ [Natural]
--- sizeList = [1..10] <> [20]
-sizeList = [1..4]
+sizeList = [1..10] ⊕ [20]
+-- sizeList = [1..6]
 
-app
-    ∷ Natural
-        -- ^ number of times 'T.toFoldCase' is called
-    → T.Text
-app = app1
+-- -------------------------------------------------------------------------- --
+-- Supported GHC Versions
+
+data GhcVersion
+    = GHC_7_6_3
+    | GHC_7_10_1
+    | GHC_7_8_3
+    deriving (Show, Read, Eq, Ord, Bounded, Enum, Typeable)
+
+readGhcVersion
+    ∷ (IsString e, MonadError e m, IsString a, Eq a, Show a, Monoid e)
+    ⇒ a
+    → m GhcVersion
+readGhcVersion "7.6.3" = return GHC_7_6_3
+readGhcVersion "7.8.3" = return GHC_7_8_3
+readGhcVersion "7.10.1" = return GHC_7_10_1
+readGhcVersion x = throwError $ "unsupported GHC Version: " ⊕ sshow x
+
+ghcVersionText
+    ∷ IsString a
+    ⇒ GhcVersion
+    → a
+ghcVersionText GHC_7_6_3 = "7.6.3"
+ghcVersionText GHC_7_8_3 = "7.8.3"
+ghcVersionText GHC_7_10_1 = "7.10.1"
+
+pGhcVersion ∷ O.Parser GhcVersion
+pGhcVersion = O.option (O.eitherReader readGhcVersion)
+    $ O.long "ghc-version"
+    ⊕ O.short 'g'
+    ⊕ O.metavar "7.6.3|7.8.3|7.10.1"
+    ⊕ O.help "ghc version that is included in the tests"
+
+-- -------------------------------------------------------------------------- --
+-- Optimization Flag
+
+data Optimization
+    = O0
+    | O1
+    | O2
+    deriving (Show, Read, Eq, Ord, Bounded, Enum, Typeable)
+
+readOptimization
+    ∷ (IsString e, MonadError e m, IsString a, Eq a, Show a, Monoid e)
+    ⇒ a
+    → m Optimization
+readOptimization "O0" = return O0
+readOptimization "O" = return O1
+readOptimization "O2" = return O2
+readOptimization x = throwError $ "unsupported optimization level: " ⊕ sshow x
+
+optimizationText
+    ∷ IsString a
+    ⇒ Optimization
+    → a
+optimizationText O0 = "O0"
+optimizationText O1 = "O"
+optimizationText O2 = "O2"
+
+pOptimization ∷ O.Parser Optimization
+pOptimization = O.option (O.eitherReader readOptimization)
+    $ O.long "optimization"
+    ⊕ O.short 'o'
+    ⊕ O.metavar "O0|O|O2"
+    ⊕ O.help "optimization level that is used in the tests"
+    ⊕ O.value O1
+
+-- -------------------------------------------------------------------------- --
+-- Test Applications
+
+data TestApp
+    = App0
+    | App1
+    deriving (Show, Read, Eq, Ord, Bounded, Enum, Typeable)
+
+readTestApp
+    ∷ (IsString e, MonadError e m, IsString a, Eq a, Show a, Monoid e)
+    ⇒ a
+    → m TestApp
+readTestApp "app0" = return App0
+readTestApp "app1" = return App1
+readTestApp x = throwError $ "unknown test application: " ⊕ sshow x
+
+testAppText
+    ∷ IsString a
+    ⇒ TestApp
+    → a
+testAppText App0 = "app0"
+testAppText App1 = "app1"
+
+pTestApp ∷ O.Parser TestApp
+pTestApp = O.option (O.eitherReader readTestApp)
+    $ O.long "test-app"
+    ⊕ O.short 'a'
+    ⊕ O.metavar "app0|app1"
+    ⊕ O.help "test application"
+
+-- -------------------------------------------------------------------------- --
+-- Parameter
+
+data Params = Params
+    { _paramTestApp ∷ !TestApp
+    , _paramGhcVersions ∷ ![GhcVersion]
+    , _paramOptimization ∷ Optimization
+    , _paramSizes ∷ ![Natural]
+    }
+    deriving (Show, Read, Eq, Ord, Typeable)
+
+pParams ∷ O.Parser Params
+pParams = Params
+    <$> pTestApp
+    <*> many pGhcVersion
+    <*> pOptimization
+    <*> O.option (O.eitherReader pSizes)
+        ( O.long "sizes"
+        ⊕ O.short 's'
+        ⊕ O.metavar "RANGE(,RANGE)*"
+        ⊕ O.help "list of sizes of the test application"
+        ⊕ O.value [1]
+        )
+
+pSizes ∷ String → Either String [Natural]
+pSizes s = either (Left ∘ show) Right $ P.parse ranges "sizes option" s
+  where
+    ranges = concat <$> P.sepBy1 numberOrRange (P.char ',')
+    numberOrRange = P.try range <|> (:[]) <$> natural
+    range = (\a b → [a..b]) <$> natural <*> (P.string ".." *> natural)
+
+    natural = fromIntegral <$> P.natural (P.makeTokenParser P.haskellDef)
 
 -- -------------------------------------------------------------------------- --
 -- main
 
 main ∷ IO ()
 main = do
-    versions ← getArgs >>= \case
-        [] → return defaultVersions
-        x → return $ T.pack <$> x
-    mapM_ (benchGhcVersion sizeList) versions
+    params ← O.execParser $ O.info pParams
+        $ O.briefDesc
+        ⊕ O.progDesc ""
+    benchGhcVersion $ params
+        { _paramGhcVersions = if null (_paramGhcVersions params)
+            then defaultVersions
+            else _paramGhcVersions params
+        }
 
 benchGhcVersion
-    ∷ [Natural]
-        -- ^ list with test cases (number of times 'T.toFoldCase' is called)
-    → T.Text
-        -- ^ compiler version
+    ∷ Params
     → IO ()
-benchGhcVersion l ver = do
-    T.putStrLn $ "# ghc-" <> ver
-    mapM_ (\i → bench i ver >>= printTime i) l
-    T.putStrLn "\n"
+benchGhcVersion Params{..} = do
+    forM_ _paramGhcVersions $ \ver → do
+        T.putStrLn $ "# ghc-" ⊕ ghcVersionText ver
+        forM_ _paramSizes $ \size → do
+            bench _paramTestApp _paramOptimization size ver >>= uncurry (printResults size)
+        T.putStrLn "\n"
   where
-    printTime i t = T.putStrLn $  sshow i <> " " <> sshow (fromIntegral (timeSpecAsNanoSecs t) / 1e9 ∷ Double)
+    printResults i t s = T.putStrLn ∘ T.intercalate " " $
+        [ sshow i
+        , sshow (fromIntegral (timeSpecAsNanoSecs t) / 1e9 ∷ Double)
+        , sshow (fromIntegral s / 1e6 ∷ Double)
+        ]
 
 bench
-    ∷ Natural
+    ∷ TestApp
+    → Optimization
+    → Natural
         -- ^ size
-    → T.Text
+    → GhcVersion
         -- ^ compiler version
-    → IO TimeSpec
-bench size ver = withTempDirectory normal "." "ghc-bench" $ \dir → do
-    let fileName = dir </> "Main.hs"
-    T.writeFile fileName $ app size
-    compile ver fileName
+    → IO (TimeSpec, Natural)
+bench app opt size ver = withTempDirectory normal "." "ghc-bench" $ \dir → do
+    let baseName = dir </> "Main"
+        fileName = baseName <.> "hs"
+        binName = baseName
+    T.writeFile fileName $ getApp app size
+    t ← compile opt ver baseName
+    s ← (fromIntegral ∘ fileSize) <$> getFileStatus binName
+    return (t, s)
 
 compile
-    ∷ T.Text
+    ∷ Optimization
+    → GhcVersion
         -- ^ compiler version
     → FilePath
-        -- ^ Filename
+        -- ^ File name /without extension/
     → IO TimeSpec
-compile ver fileName = fmap fst . time $
-    callCommand $ "ghc-" <> T.unpack ver <> " -fforce-recomp " <> optimizationFlag <> " " <> fileName <> " >/dev/null"
+compile opt ver baseName = do
+    fmap fst ∘ time $
+        callCommand
+            $ "ghc-" ⊕ ghcVersionText ver
+            ⊕ " -fforce-recomp"
+            ⊕ " -" ⊕ optimizationText opt
+            ⊕ " " ⊕ hsName
+            ⊕ " >/dev/null"
+  where
+    hsName = baseName <.> "hs"
+
+getApp ∷ TestApp → Natural → T.Text
+getApp App0 = app0
+getApp App1 = app1
 
 -- |
 -- > main = do
@@ -103,17 +266,18 @@ app0
     → T.Text
 app0 n = T.unlines $
     [ "{-# LANGUAGE OverloadedStrings #-}"
+    , "{-# LANGUAGE UnicodeSyntax #-}"
     , "module Main"
     , "( main"
     , ") where"
     , ""
     , "import qualified Data.Text as T"
     , ""
-    , "main :: IO ()"
+    , "main ∷ IO ()"
     , "main = do"
     ]
-    <> replicate (fromIntegral n) "    print (toCaseFold \"a\")"
-    -- <> replicate (fromIntegral n) "    print (toLower \"a\")"
+    ⊕ replicate (fromIntegral n) "    print (T.toCaseFold \"a\")"
+    -- ⊕ replicate (fromIntegral n) "    print (toLower \"a\")"
 
 -- |
 -- > import qualified Data.Text as T
@@ -129,23 +293,24 @@ app1
     → T.Text
 app1 n = T.unlines $
     [ "{-# LANGUAGE OverloadedStrings #-}"
+    , "{-# LANGUAGE UnicodeSyntax #-}"
     , "module Main"
     , "( main"
     , ") where"
     , ""
     , "import qualified Data.Text as T"
     , ""
-    , "main :: IO ()"
+    , "main ∷ IO ()"
     , "main = print"
     ]
-    <> replicate (fromIntegral n) "    $ T.toCaseFold . T.take 1"
-    <> [ "    $ \"abc\"" ]
+    ⊕ replicate (fromIntegral n) "    $ T.toCaseFold . T.take 1"
+    ⊕ [ "    $ \"abc\"" ]
 
 -- -------------------------------------------------------------------------- --
 -- Utils
 
 sshow ∷ (Show a, IsString b) ⇒ a → b
-sshow = fromString . show
+sshow = fromString ∘ show
 
 time
     ∷ IO a
--- plot.gnuplot
+++ plot.gnuplot
@@ -1,24 +1,43 @@
-set terminal png
-set xlabel "number of nested calls to Data.Text.toCaseFold"
+# Parameters
+id = "2015-04-27"
+appid = "app0"
+opt = "O2"
+
+# Generic settings
+set terminal png size 1024,480
+set xlabel "number of calls to Data.Text.toCaseFold"
 set ylabel "build time in seconds"
+set y2label "binary size in MB"
+set ytics nomirror
+set y2tics
+set key outside
 
-# app1 -O0
-# set title "Build of app1 with -O0"
-# set label "main :: IO ()\nmain = print\n    -- repeated n times:\n    $ toCaseFold . take 1\n    $ \"abc\"" at 2,1
-# plot "data/results-app1-O0.txt" index 0 with linespoints title "ghc-7.6.3" \
-#      , "data/results-app1-O0.txt" index 1 with linespoints title "ghc-7.8.3" \
-#      , "data/results-app1-O0.txt" index 2 with linespoints title "ghc-7.10.1"
+# line styles
+set style line 1 lt 5 lw 2 pt 5 ps 1 lc rgb "red"
+set style line 2 lt 5 lw 2 pt 5 ps 1 lc rgb "blue"
+set style line 3 lt 5 lw 2 pt 5 ps 1 lc rgb "green"
 
-# app1 -O
-# set title "Build of app1 with -O"
-# set label "main :: IO ()\nmain = print\n    -- repeated n times:\n    $ toCaseFold . take 1\n    $ \"abc\"" at 1.5,700
-# plot "data/results-app1-O.txt" index 0 with linespoints title "ghc-7.6.3" \
-#      , "data/results-app1-O.txt" index 1 with linespoints title "ghc-7.8.3" \
-#      , "data/results-app1-O.txt" index 2 with linespoints title "ghc-7.10.1"
+set style line 4 lt 2 lw 1 pt 2 ps 1 lc rgb "red"
+set style line 5 lt 2 lw 1 pt 2 ps 1 lc rgb "blue"
+set style line 6 lt 2 lw 1 pt 2 ps 1 lc rgb "green"
 
-# app1 -O2
-set title "Build of app1 with -O2"
-set label "main :: IO ()\nmain = print\n    -- repeated n times:\n    $ toCaseFold . take 1\n    $ \"abc\"" at 1.5,600
-plot "data/results-app1-O2.txt" index 0 with linespoints title "ghc-7.6.3" \
-     , "data/results-app1-O2.txt" index 1 with linespoints title "ghc-7.8.3" \
-     , "data/results-app1-O2.txt" index 2 with linespoints title "ghc-7.10.1"
+# programs
+app0 = "main :: IO ()\nmain =\n    -- repeated n times:\n    print (toCaseFold \"a\")"
+app1 = "main :: IO ()\nmain = print\n    -- repeated n times:\n    $ toCaseFold . take 1\n    $ \"abc\""
+
+# functions
+outfile(a,o,x) = "images/results-".a."-".o.".".x.".png"
+infile(a,o,x) = "data/results-".a."-".o.".".x.".txt"
+title(a,o) = "Build of ".a." with -".o
+
+set output outfile(appid,opt,id)
+set title title(appid,opt)
+set label app0 left at screen 0.82, screen 0.5
+plot \
+      infile(appid,opt,id) index 0 using 1:2 ls 1 with linespoints title "time ghc-7.6.3" axes x1y1 \
+    , infile(appid,opt,id) index 0 using 1:3 ls 4 with linespoints title "size ghc-7.6.3" axes x1y2 \
+    , infile(appid,opt,id) index 1 using 1:2 ls 2 with linespoints title "time ghc-7.8.3" axes x1y1 \
+    , infile(appid,opt,id) index 1 using 1:3 ls 5 with linespoints title "size ghc-7.8.3" axes x1y2 \
+    , infile(appid,opt,id) index 2 using 1:2 ls 3 with linespoints title "time ghc-7.10.1" axes x1y1 \
+    , infile(appid,opt,id) index 2 using 1:3 ls 6 with linespoints title "size ghc-7.10.1" axes x1y2
+
